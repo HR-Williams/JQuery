923a633c45aed3d6fcc48eda5cce51bf9dbc510b
HR Williams <halrubinwilliams@gmail.com>
2021-01-30 23:47:54 -0800

    Refactor with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
+// $(document).ready(function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   });
+
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+
+//   $("img").click(function() {
+//     alert("This is an image.");
+//   });
+// });
+
+
+
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
+  const elementArray = ["h1", "p", "img"]
+  
+  elementArray.forEach(function(element) {
+    $(element).click(function() {
+      alert("This is a " + element);
+    });
   });
 });
